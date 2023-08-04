f4cf7a26d479c7ee5f441dd5871d72d4855610b0
Imre NAGY <crash@veluna.eu>
2023-08-04 16:06:33 +0200

    Add some package and offline install scripts

--- python/{{ cookiecutter.project_slug }}/README.md
+++ python/{{ cookiecutter.project_slug }}/README.md
@@ -12,14 +12,20 @@
 
 ## Basic commands to manage project
 
-- Install for use: `poetry install --without dev --sync`
-    - Install from package with pip: `python -m pip install <package-name.whl>`
-- Install for development: `poetry install --sync`
-    - Run test: `poetry run python -m pytest`
-    - Run test and generate html coverage report: `poetry run python -m pytest --cov-report html`
-    - Run typecheck: `poetry run python -m mypy`
+- Install for use
+    - Poetry: `poetry install --without dev --sync`
+    - Pip from package (recommended to do it in a virtual environment)
+        1. Create a virtualenv: `python -m venv .venv`
+        2. Activate virtualenv: `source .venv/Scripts/activate`
+        3. Install: `python -m pip install <package-name.whl>`
+- Install for development
+    - Install: `poetry install --sync`
     - Create package: `poetry build`
-    - Create offline-install package: `./tools/create-offline-package.sh`
+    - Create offline-install package into 'release' directory: `./tools/create-offline-package.sh`
+    - Run test: `poetry run python -m pytest`
+        - Run test and generate html coverage report: `poetry run python -m pytest --cov=src --cov-report term --cov-report html`
+    - Run mypy typecheck: `poetry run python -m mypy`
+    - Run flake8 style check: `poetry run python -m flake8 ./src`
 
 
 ## Example run
@@ -37,6 +43,15 @@ If you have one, you can explicitly use it via the "env use" command or install 
 py -{{ cookiecutter.python_version }} -m venv .venv
 poetry install --sync
 ```
+
+
+### `The Poetry configuration is invalid: - [source.0] Additional properties are not allowed ('priority' was unexpected)`
+
+Update poetry with one of these commands:
+- `poetry self update`
+- `(Invoke-WebRequest -Uri https://install.python-poetry.org -UseBasicParsing).Content | py -`
+- `curl -sSL https://install.python-poetry.org | python -`
+
 
 ## Additional links
 
--- python/{{ cookiecutter.project_slug }}/tools/create-offline-package.sh
+++ python/{{ cookiecutter.project_slug }}/tools/create-offline-package.sh
@@ -10,16 +10,66 @@ mkdir -p "${OUTPUT_DIR}"
 poetry build --format=wheel
 poetry export --only=main --without-hashes --without-urls -o "${OUTPUT_DIR}"/requirements.txt
 poetry run python -m pip download --requirement "${OUTPUT_DIR}"/requirements.txt --only-binary=:all: --dest "${OUTPUT_DIR}"/packages
-# running from CI maybe needs to specify platform and version
-# poetry run python -m pip download --requirement "${OUTPUT_DIR}"/requirements.txt --only-binary=:all: --dest "${OUTPUT_DIR}"/packages --platform win_amd64 --python-version 38
+# running from CI maybe needs to specify platform and version: --platform win_amd64 --python-version 38
 
 cp ./dist/{{ cookiecutter.package_name }}*.whl "${OUTPUT_DIR}"/
 
 cat << EOF > "${OUTPUT_DIR}"/install-user.cmd
-# NOTE: For On-Demand Desktop (ODD) usage remove '-user' because everyone uses the same global install
 python -m pip install --user --no-index --find-links ./packages -r requirements.txt
 python -m pip install --user --no-index --no-dependencies --force-reinstall --find-links . {{ cookiecutter.package_name }}
 
 @echo off
 pause
 EOF
+
+cat << EOF > "${OUTPUT_DIR}"/install-system.cmd
+SET script_path=%~dp0
+cd %script_path%
+
+python -m pip install --no-index --find-links ./packages -r requirements.txt
+python -m pip install --no-index --no-dependencies --force-reinstall --find-links . {{ cookiecutter.package_name }}
+
+@echo off
+pause
+EOF
+
+cat << EOF > "${OUTPUT_DIR}"/install-virtualenv.cmd
+python -m venv .venv
+
+.\.venv\Scripts\python.exe -m pip install --no-index --find-links ./packages -r requirements.txt
+.\.venv\Scripts\python.exe -m pip install --no-index --no-dependencies --force-reinstall --find-links . {{ cookiecutter.package_name }}
+
+@echo off
+pause
+EOF
+
+cat << EOF > "${OUTPUT_DIR}"/start-virtualenv.cmd
+start .\.venv\Scripts\activate.bat
+EOF
+
+
+cat << 'EOF' > "${OUTPUT_DIR}"/README.md.txt
+# {{ cookiecutter.project_name }} OnDemand Desktop (ODD) install package
+
+## Install
+
+**The recommended installation method is the virtual environment.**
+
+You can install it in different ways:
+- **Install inside a new virtual environment** (virtual environment created automatically in the .venv/ directory): `install-virtualenv.cmd`
+- Install system wide: `install-system.cmd`
+- Install only for the current user: `install-user.cmd`
+
+
+## Usage
+
+### In virtual environment
+
+If you installed the app into a virtual environment you can open a command prompt into the virtual environment with `start-virtualenv.cmd` and use the command from the [Other install methods](#other-install-methods) section. Or you can run directly with the command `.\.venv\Scripts\python.exe -m {{ cookiecutter.package_name }} -h` without opening command prompt to the virtual environment.
+
+
+### Other install methods
+
+After installation you can test it with the command `python -m {{ cookiecutter.package_name }} -h` which shows the command line help message.
+EOF
+
